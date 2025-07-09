18afb5f20b848f96e00fd848e68aeefe0651e384
solomonbillot.cssolutions <solomonbillot.cssolutions@gmail.com>
2025-07-09 14:14:08 +0000

    dashboard-layouts and links

--- nextjs-dashboard/app/Dashboard/layout.tsx
+++ nextjs-dashboard/app/Dashboard/layout.tsx
@@ -1,5 +1,12 @@
+import Sidenav from "../ui/dashboard/sidenav";
+
 export default function Layout({children}: {children: React.ReactNode}) {
     return (
-      <div className="flex h-screen flex-col md:flex-row bg-black "></div>
+      <div className="flex h-screen flex-col md:flex-row md:overflow-hidden ">
+        <div className="w-full flex-none md:w-64">
+            <Sidenav/>
+        </div>
+        <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      </div>
     );
 }
